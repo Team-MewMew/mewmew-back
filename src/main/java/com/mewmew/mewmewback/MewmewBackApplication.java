7375f598f3a395895069ceece4488648c467c6c6
95Donguk <midgard95@nate.com>
2024-01-11 23:19:33 +0900

    :wrench: init: Initial commit

---
+++ src/main/java/com/mewmew/mewmewback/MewmewBackApplication.java
@@ -0,0 +1,13 @@
+package com.mewmew.mewmewback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MewmewBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MewmewBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mewmew/mewmewback/MewmewBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mewmew.mewmewback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MewmewBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
