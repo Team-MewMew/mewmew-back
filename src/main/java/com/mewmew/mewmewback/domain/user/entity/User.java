4dd298918aa77fef2edd271f7e93698cffc9cb43
95Donguk <midgard95@nate.com>
2024-02-06 04:05:01 +0900

    :sparkles: feat: User Entity 작성

--- src/main/java/com/mewmew/mewmewback/domain/user/entity/User.java
+++ src/main/java/com/mewmew/mewmewback/domain/user/entity/User.java
@@ -1,4 +1,33 @@
 package com.mewmew.mewmewback.domain.user.entity;
 
-public class User {
+import com.mewmew.mewmewback.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
+
+@Entity
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_no", nullable = false, unique = true, updatable = false)
+    private Long userNo;
+
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    @Column(name = "user_del_flag", columnDefinition = "TINYINT(1)", nullable = false)
+    @ColumnDefault("0")
+    @Comment("탈퇴여부 확인 0: 활성, 1: 탈퇴")
+    private Boolean userDelFlag;
 }
